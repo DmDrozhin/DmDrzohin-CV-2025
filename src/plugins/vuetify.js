c6ec164956fcaf9aa4ac66686d7a99fc5509aaad
Твоё имя < email  GitHub>
2025-09-12 13:58:33 +0300

    DM-2509 custom breakpoints for scss

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,7 @@
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-import { customTheme, customDisplay, defaults } from './vuetify.config';
+import { customTheme, defaults } from './vuetify.config';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 // This is where you can customize your theme
 // https://vuetifyjs.com/en/features/theme/
@@ -9,8 +9,7 @@ export default createVuetify({
   components,
   directives,
   theme: customTheme,
-  display: customDisplay,
-  defaults: defaults,
+  defaults: defaults, 
   icons: {
     defaultSet: 'mdi',
     aliases,
