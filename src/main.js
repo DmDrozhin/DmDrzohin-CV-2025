b49e0de3def669ccce70fc118471aa9a99508200
Твоё имя < email  GitHub>
2025-11-12 11:39:10 +0200

    MAIN favicon updated with new version

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue';
 import { createPinia } from 'pinia';
 import vuetify from '@/plugins/vuetify.js';
 import '@/styles/global.scss';
-// import '@mdi/font/css/materialdesignicons.css';
 const pinia = createPinia();
 
 createApp(App).use(vuetify).use(pinia).mount('#app');
